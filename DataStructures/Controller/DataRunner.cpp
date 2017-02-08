934411d03d70196237cd29e7b89c5be0bb6e90d0
Pbra1484 <pbra1484@740s-pl-123.local>
2017-02-08 13:26:42 -0700

    made runner

---
+++ DataStructures/Controller/DataRunner.cpp
@@ -0,0 +1,16 @@
+//
+//  DataRunner.cpp
+//  DataStructures
+//
+//  Created by Brashear, Patrick on 2/8/17.
+//  Copyright © 2017 Brashear, Patrick. All rights reserved.
+//
+
+#include "DataStructureController.hpp"
+
+int main()
+{
+    DataStructureController * app = new DataStructureController();
+    app->start();
+    return 0;
+}
