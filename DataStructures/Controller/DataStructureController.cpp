7f8cef181a69f380c1c0155460daebecbba968ba
Pbra1484 <pbra1484@740s-pl-123.local>
2017-02-08 13:11:21 -0700

    made dataStructureController hpp

---
+++ DataStructures/Controller/DataStructureController.cpp
@@ -0,0 +1,15 @@
+//
+//  DataStructureController.cpp
+//  DataStructures
+//
+//  Created by Brashear, Patrick on 2/8/17.
+//  Copyright © 2017 Brashear, Patrick. All rights reserved.
+//
+
+#include "DataStructureController.hpp"
+
+
+DataStructureController :: DataStructureController()
+{
+    
+}
---
+++ DataStructures/Controller/DataStructureController.hpp
@@ -0,0 +1,22 @@
+//
+//  DataStructureController.hpp
+//  DataStructures
+//
+//  Created by Brashear, Patrick on 2/8/17.
+//  Copyright © 2017 Brashear, Patrick. All rights reserved.
+//
+
+#ifndef DataStructureController_hpp
+#define DataStructureController_hpp
+
+class DataStructureController
+{
+private:
+    void testIntArray();
+    
+public:
+    DataStructureController();
+    void start();
+};
+
+#endif /* DataStructureController_hpp */
