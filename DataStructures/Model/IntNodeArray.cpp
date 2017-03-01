af0a1e3e0bb4db32077003229baf6be99f38d2af
Pbra1484 <pbra1484@740s-pl-123.local>
2017-03-01 12:48:59 -0700

    updated UML

--- DataStructures/Model/IntNodeArray.cpp
+++ DataStructures/Model/IntNodeArray.cpp
@@ -9,16 +9,14 @@
 #include "IntNodeArray.hpp"
 #include <assert.h>
 
-IntNodeArray :: IntNodeArray(int size)
-{
+IntNodeArray :: IntNodeArray(int size){
     assert(size > 0);
     
     this->size = size;
     this->front = new IntNode();
     
     
-    for(int index = 1; index < size; index++)
-    {
+    for(int index = 1; index < size; index++){
         IntNode * current = new IntNode();
         current->setNodePointer(front);
         front = current;
@@ -26,15 +24,13 @@ IntNodeArray :: IntNodeArray(int size)
     
 
 }
-int IntNodeArray :: getFromIndex(int index)
-{
+int IntNodeArray :: getFromIndex(int index){
     assert(index >= 0 && index < size);
     int value = 0;
     
     IntNode * current = front;
     
-    for(int position = 0; position < index; position++)
-    {
+    for(int position = 0; position < index; position++){
         current = current->getNodePointer();
     }
     
@@ -43,14 +39,12 @@ int IntNodeArray :: getFromIndex(int index)
     return value;
 }
 
-void IntNodeArray :: setAtIndex(int index, int value)
-{
+void IntNodeArray :: setAtIndex(int index, int value){
     assert(index >= 0 && index < size);
     
     IntNode * current = front;
     
-    for(int position = 0; position < index; position++)
-    {
+    for(int position = 0; position < index; position++){
         current = current->getNodePointer();
     }
     
