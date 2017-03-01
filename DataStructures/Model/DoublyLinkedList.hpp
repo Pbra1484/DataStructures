718bbc352ccf4f73ad7f2bedfd8d3c1b3f674853
Pbra1484 <pbra1484@740s-pl-123.local>
2017-03-01 13:15:17 -0700

    implemented DoublyLinkedList

--- DataStructures/Model/DoublyLinkedList.hpp
+++ DataStructures/Model/DoublyLinkedList.hpp
@@ -14,8 +14,46 @@
 template <class Type>
 class DoublyLinkedList
 {
+private:
+    BiDirectionalNode<Type> * front;
+    BiDirectionalNode<Type> * end;
+    int size;
+public:
+    DoublyLinkedList();
+    virtual ~DoublyLinkedList();
+    int getSize() const;
+    BiDirectionalNode<Type> * getFront() const;
+    BiDirectionalNode<Type> * getEnd() const;
     
+    virtual void add(Type data) = 0;
+    virtual Type remove(int index) = 0;
 };
+
+template <class Type>
+DoublyLinkedList<Type> :: DoublyLinkedList()
+{
+    size = 0;
+    front = nullptr;
+    end = nullptr;
+}
+
+template <class Type>
+int DoublyLinkedList<Type> :: getSize()
+{
+    return szie;
+}
+
+template <class Tyupe>
+BiDirectionalNode<Type> * DoublyLinkedList<Type> :: getFront()
+{
+    return front;
+}
+
+template <class Tyupe>
+BiDirectionalNode<Type> * DoublyLinkedList<Type> :: getEnd()
+{
+    return end;
+}
 
 #endif /* DoublyLinkedList_h */
 
