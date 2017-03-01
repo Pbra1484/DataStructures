fef4382d47b4a05c4e03400c612d116461879540
Pbra1484 <pbra1484@740s-pl-123.local>
2017-03-01 12:52:10 -0700

    Began DoublyLinkedList.hpp

---
+++ DataStructures/Model/DoublyLinkedList.hpp
@@ -0,0 +1,22 @@
+//
+//  DoublyLinkedList.hpp
+//  DataStructures
+//
+//  Created by Brashear, Patrick on 3/1/17.
+//  Copyright © 2017 Brashear, Patrick. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include "BiDirectionalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+    
+};
+
+#endif /* DoublyLinkedList_h */
+
+
